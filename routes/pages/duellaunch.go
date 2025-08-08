ece88ce81fb99831c49e7ff222b74735e7e0da65
Damonsas <danh.kh.pro@gmail.com>
2025-08-08 18:34:56 +0200

    Update duellaunch.go

--- routes/pages/duellaunch.go
+++ routes/pages/duellaunch.go
@@ -105,6 +105,9 @@ func DisplayDuel(w http.ResponseWriter, r *http.Request) {
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<title>Duel: {{.Duel.Name}}</title>
 			<link rel="stylesheet" href="/asset/scss/style.css">
+			<script src="/asset/js/vocal.js"></script>
+    		<script type="module" src="/asset/js/duel.js"></script>
+    		<script type="module" src="/asset/js/ui.js"></script>
 		</head>
 		<body>
     <div class="duelContainer">
